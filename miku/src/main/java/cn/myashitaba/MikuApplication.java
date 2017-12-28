da82eb5a143b03d59aec34eaf8a49d69e38470e3
Administrator <Administrator@W7X64-20151009I>
2017-12-28 19:15:21 +0800

    first commit

---
+++ miku/src/main/java/cn/myashitaba/MikuApplication.java
@@ -0,0 +1,12 @@
+package cn.myashitaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikuApplication.class, args);
+	}
+}
